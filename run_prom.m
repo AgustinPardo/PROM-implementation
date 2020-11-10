c3525483b2194c181e17486a37f7dd9b127b7702
AgustinPardo <agu_pardo@hotmail.com>
2020-11-10 16:42:17 -0300

    PROM version 2

--- run_prom.m
+++ run_prom.m
@@ -1,6 +1,5 @@
-clear;
 
-load('/home/agustin/cobratoolbox/PROM_Chandrasekaran/mtbpromdata.mat');
+load('/home/agustin/FBA_Tesis/PROM_Chandrasekaran/mtbpromdata.mat');
 
 fid = fopen('targets_filter.txt','r');
 data = textscan(fid,'%s', 'Delimiter', '\n');
@@ -35,10 +34,28 @@ addpath('/home/agustin/cobratoolbox/PROM_Chandrasekaran');
 %[f,v,status1,lostxns] = PROM(model, expression, expressionid, regulator, targets, litevidence,prob_prior);
 
 %PROM con nuevos datos
-[f,v,status1,lostxns] = PROM(iEK1008, expression, expressionid, z_regulator, z_targets, z_litevidence, z_prob_prior);
+%[f,v,status1,lostxns] = PROM(iEK1008, expression, expressionid, z_regulator, z_targets, z_litevidence, z_prob_prior);
+
+%promv2
+%function [f,f_ko,v,v_ko,status1,lostxns,probtfgene] =  promv2(model,expression,expressionid,regulator,targets,litevidence,prob_prior,subsets,v11,v12,KAPPA,DATATHRESHVAL,probtfgene,sizeflag)
+[v11, v12] = fastFVA(iEK1008);
+[f,f_ko,v,v_ko,status1,lostxns,probtfgene] =  promv2(iEK1008,expression,expressionid,z_regulator,z_targets,z_litevidence,z_prob_prior,[],v11,v12,[],[],[],1)
 
 
-%fid = fopen('expressionid.txt','w');
-%CT = expressionid;
+%Write the outputs in files
+%fid = fopen('f.txt','w');
+%CT = f;
 %fprintf(fid,'%s\n', CT{:});
 %fclose(fid);
+
+%dlmwrite('f.txt', f, 'delimiter','\t','newline','pc','precision',13);
+
+%analisis de resultados
+diff_f_fko= ((f - f_ko)/0.0582)*100;
+u_z_regulator=unique(z_regulator);
+T = array2table(diff_f_fko,'VariableNames',u_z_regulator);
+
+YourArray = table2array(T);
+Tt = array2table(YourArray.');
+Tt.Properties.RowNames = T.Properties.VariableNames;
+writetable(Tt,"reguladores_diff_t_tko.txt",'WriteRowNames',true);
