d2221cf19febc105707f032d888cf834e1908b62
AgustinPardo <agu_pardo@hotmail.com>
2020-11-11 19:27:27 -0300

    Add reaction to model

--- run_prom.m
+++ run_prom.m
@@ -1,4 +1,8 @@
 
+%Corro CobraToolBox
+%initCobraToolbox(false);
+
+% Cargo los datos del paper de PROM
 load('/home/agustin/FBA_Tesis/PROM_Chandrasekaran/mtbpromdata.mat');
 
 fid = fopen('targets_filter.txt','r');
@@ -16,7 +20,20 @@ z_prob_prior = importfile_numeric('prob_prior_filter.txt','r');
 
 clear ans fid data;
 
+% Cargo el Modelo de BIGG
 load('iEK1008.mat');
+
+% Biomasa
+%checkObjective(iEK1008)
+%printRxnFormula(iEK1008,'BIOMASS__2');
+
+% Creo la nueva reaccion de Biomasa
+model = addReaction(model, 'GAPDH',...
+'reactionFormula', 'g3p[c] + nad[c] + 2 pi[c] -> nadh[c] + h[c] + 13bpg[c]');
+
+% Cambio la biomasa
+%iEK1008 = changeObjective(iEK1008,'Biomass_Ecoli_core_w_GAM');
+
 
 rev=iEK1008.rev;
 
@@ -42,6 +59,7 @@ addpath('/home/agustin/cobratoolbox/PROM_Chandrasekaran');
 [f,f_ko,v,v_ko,status1,lostxns,probtfgene] =  promv2(iEK1008,expression,expressionid,z_regulator,z_targets,z_litevidence,z_prob_prior,[],v11,v12,[],[],[],1)
 
 
+%% Exporto variables
 %Write the outputs in files
 %fid = fopen('f.txt','w');
 %CT = f;
@@ -50,7 +68,7 @@ addpath('/home/agustin/cobratoolbox/PROM_Chandrasekaran');
 
 %dlmwrite('f.txt', f, 'delimiter','\t','newline','pc','precision',13);
 
-%analisis de resultados
+%% Analisis de resultados
 diff_f_fko= ((f - f_ko)/0.0582)*100;
 u_z_regulator=unique(z_regulator);
 T = array2table(diff_f_fko,'VariableNames',u_z_regulator);
@@ -58,4 +76,4 @@ T = array2table(diff_f_fko,'VariableNames',u_z_regulator);
 YourArray = table2array(T);
 Tt = array2table(YourArray.');
 Tt.Properties.RowNames = T.Properties.VariableNames;
-writetable(Tt,"reguladores_diff_t_tko.txt",'WriteRowNames',true);
+%writetable(Tt,"reguladores_diff_t_tko.txt",'WriteRowNames',true);
