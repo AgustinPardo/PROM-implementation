ebf06e158c82eda21a569e8ae6850797ec7dd3d5
AgustinPardo <agu_pardo@hotmail.com>
2020-11-16 16:42:57 -0300

    Comparamos 3 medios del paper iEK1011 y resultado PROM

--- run_prom.m
+++ run_prom.m
@@ -23,30 +23,30 @@ z_prob_prior = importfile_numeric('prob_prior_filter.txt','r');
 
 clear ans fid data;
 
-
 % Cargo el Modelo de BIGG, Soluciono problemade rules y rev.
-load('iEK1008.mat')
-rev=iEK1008.rev; 
-clear ans iEK1008; 
-iEK1008=readCbModel('iEK1008.mat'); 
-iEK1008.rev=rev; 
+load('/home/agustin/FBA_Tesis/PROM_trabajo/convertion/iEK1011_m7H10_media.mat')
+rev=iEK1011.rev;
+clear ans iEK1011; 
+iEK1011=readCbModel('/home/agustin/FBA_Tesis/PROM_trabajo/convertion/iEK1011_griffinEssen_media.mat'); 
+iEK1011.rev=rev; 
 clear rev;
 
 % Chequeo Biomasa actual
-checkObjective(iEK1008)
+checkObjective(iEK1011)
 %printRxnFormula(iEK1008,'BIOMASS__2');
 
+FBAsolution = optimizeCbModel(iEK1011,'max')
 %% Cargo otro modelo, veo su reaccion de biomasa y la guardo
-    load('/home/agustin/FBA_Tesis/PROM_trabajo/convertion/iEK1011_m7H10_media.mat');
-    biomass = checkObjective(iEK1011);
-    biomass_rxn_c = char(printRxnFormula(iEK1011,biomass));
+    %load('/home/agustin/FBA_Tesis/PROM_trabajo/convertion/iEK1011_m7H10_media.mat');
+    %biomass = checkObjective(iEK1011);
+    %biomass_rxn_c = char(printRxnFormula(iEK1011,biomass));
 
 % Cambio la biomasa
     %iEK1008 = addReaction(iEK1008, 'biomass_rxn_c','reactionFormula', biomass_rxn_c);
     %iEK1008 = changeObjective(iEK1008,'biomass_rxn_c');
 
 % Chequeo el cambio de Biomasa
-    checkObjective(iEK1008)
+    %checkObjective(iEK1008)
 %%
 
 addpath('/home/agustin/cobratoolbox/PROM_Chandrasekaran');
@@ -59,8 +59,8 @@ addpath('/home/agustin/cobratoolbox/PROM_Chandrasekaran');
 
 % Corro PROM version 2
 %function [f,f_ko,v,v_ko,status1,lostxns,probtfgene] =  promv2(model,expression,expressionid,regulator,targets,litevidence,prob_prior,subsets,v11,v12,KAPPA,DATATHRESHVAL,probtfgene,sizeflag)
-[v11, v12] = fastFVA(iEK1008);
-[f,f_ko,v,v_ko,status1,lostxns,probtfgene] =  promv2(iEK1008,expression,expressionid,z_regulator,z_targets,z_litevidence,z_prob_prior,[],v11,v12,[],[],[],1)
+[v11, v12] = fastFVA(iEK1011);
+[f,f_ko,v,v_ko,status1,lostxns,probtfgene] =  promv2(iEK1011,expression,expressionid,z_regulator,z_targets,z_litevidence,z_prob_prior,[],v11,v12,[],[],[],1)
 
 
 %% Exporto variables
@@ -73,11 +73,11 @@ addpath('/home/agustin/cobratoolbox/PROM_Chandrasekaran');
 %dlmwrite('f.txt', f, 'delimiter','\t','newline','pc','precision',13);
 
 %% Analisis de resultados
-diff_f= (f/0.0582)*100;
+diff_f= (f/FBAsolution.f)*100;
 u_z_regulator=unique(z_regulator);
 T = array2table(diff_f,'VariableNames',u_z_regulator);
 
 YourArray = table2array(T);
 Tt = array2table(YourArray.');
 Tt.Properties.RowNames = T.Properties.VariableNames;
-writetable(Tt,"reguladores_diff_f.txt",'WriteRowNames',true);
+writetable(Tt,"diff_f_Griffin.txt",'WriteRowNames',true);
