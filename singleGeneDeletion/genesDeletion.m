d53d56f222269ab2c9af1b90b96f7f272a341847
AgustinPardo <agu_pardo@hotmail.com>
2020-11-19 20:12:53 -0300

    Essen comparatives start to work

--- singleGeneDeletion/genesDeletion.m
+++ singleGeneDeletion/genesDeletion.m
@@ -1,2 +1,6 @@
-iEK1011 = readCbModel('/home/agustin/FBA_Tesis/PROM_trabajo/convertion/iEK1011_griffinEssen_media.mat');
+iEK1011 = readCbModel('/home/agustin/FBA_Tesis/PROM_trabajo/convertion/iEK1011_deJesusEssen_media.mat');
 [grRatio, grRateKO, grRateWT, hasEffect, delRxns, fluxSolution] = singleGeneDeletion(iEK1011);
+
+genes=iEK1011.genes;
+t=table(genes,grRateKO);
+writetable(t,"DeJesus_Essen_metabolic.txt",'WriteRowNames',true);
